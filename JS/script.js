d15753281d87c979c4b142dc84ed59c24a95b273
mukhtarabdirahman <mukhtarabdirahmanm@gmail.com>
2019-11-02 19:36:07 +0300

    jquery design

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,23 @@
+$(document).ready(function(){
+   $("#icon").click(function(){
+   $("#hide").show();
+   });
+   $("#icon").click(function(){
+   $("#icon").hide();
+   });
+   $("#hide").click(function(){
+   $("#icon").show();
+   });
+   $("#hide").click(function(){
+   $("#hide").hide();
+   });
 
+
+
+
+
+
+});
 
 
 
